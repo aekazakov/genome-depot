4aaeb1938b758510f90019c55bcc15f4210e75aa
Alexey Kazakov <aekazakov@lbl.gov>
2022-02-24 16:00:18 -0800

    Added filters

--- genomebrowser/cgcmsadmin/admin.py
+++ genomebrowser/cgcmsadmin/admin.py
@@ -1,6 +1,28 @@
+import os
+import shutil
 from django.contrib import admin
 # Import your models here
 from browser.models import Strain, Sample, Genome, Contig, Gene, Taxon, Cog_class, Kegg_reaction, Kegg_pathway, Kegg_ortholog, Go_term, Cazy_family, Ec_number, Ortholog_group, Eggnog_description, Tc_family, Strain_metadata, Sample_metadata, Protein, Annotation, Operon, Site, Regulon, Config, ChangeLog
+from browser.dataimport.importer import Importer
+
+def delete_genome(modeladmin, request, queryset):
+    importer = Importer()
+    genome_names = [genome.name for genome in queryset]
+    queryset.delete()
+    Protein.objects.filter(gene=None).delete()
+    Strain.objects.filter(genome=None).delete()
+    Sample.objects.filter(genome=None).delete()
+    importer.export_proteins()
+    importer.export_contigs()
+    importer.delete_search_databases()
+    shutil.copyfile(os.path.join(importer.config['cgcms.temp_dir'], os.path.basename(importer.config['cgcms.search_db_nucl'])), importer.config['cgcms.search_db_nucl'])
+    shutil.copyfile(os.path.join(importer.config['cgcms.temp_dir'], os.path.basename(importer.config['cgcms.search_db_prot'])), importer.config['cgcms.search_db_prot'])
+    importer.create_search_databases()
+    for genome in genome_names:
+        shutil.rmtree(os.path.join(importer.config['cgcms.json_dir'], genome))
+    os.remove(importer.config['cgcms.search_db_nucl'])
+    os.remove(importer.config['cgcms.search_db_prot'])
+delete_genome.short_description = 'Delete genome and static content'
 
 
 class CgcmsAdminSite(admin.AdminSite):
@@ -13,20 +35,37 @@ cgcms_admin_site = CgcmsAdminSite(name='cgcms_admin')
 
 # Register your models here.
 class GenomeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'taxon', 'strain', 'size', 'contigs']
+    #actions = [delete_genome]
+    list_display = ['name', 'taxon', 'strain', 'sample', 'size', 'contigs']
+    list_filter = (('strain', admin.EmptyFieldListFilter), ('sample', admin.EmptyFieldListFilter))
     ordering = ['name']
+    search_fields = ['name']
+    fields = (
+        ('name', 'taxon'),
+        ('strain', 'sample'),
+        ('contigs', 'size', 'genes'),
+        'description',
+        ('json_url', 'pub_date'),
+        ('external_id', 'external_url'),
+        'gbk_filepath'
+    )
     
 cgcms_admin_site.register(Genome, GenomeAdmin)
 
+
 class GeneAdmin(admin.ModelAdmin):
     list_display = ['locus_tag', 'genome', 'contig', 'start', 'end']
+    list_filter = ['type']
     ordering = ['locus_tag', 'genome']
+    search_fields = ['locus_tag', 'genome__name']
 
 cgcms_admin_site.register(Gene, GeneAdmin)
+
 
 class TaxonAdmin(admin.ModelAdmin):
     list_display = ['admin_name', 'parent_id', 'eggnog_taxid']
     ordering = ['name']
+    search_fields = ['name']
 
 cgcms_admin_site.register(Taxon, TaxonAdmin)
 
@@ -34,6 +73,7 @@ cgcms_admin_site.register(Taxon, TaxonAdmin)
 class StrainAdmin(admin.ModelAdmin):
     list_display = ['strain_id', 'full_name', 'order', 'taxon']
     ordering = ['strain_id']
+    search_fields = ['strain_id', 'full_name']
 
 cgcms_admin_site.register(Strain, StrainAdmin)
 
@@ -41,6 +81,7 @@ cgcms_admin_site.register(Strain, StrainAdmin)
 class SampleAdmin(admin.ModelAdmin):
     list_display = ['sample_id', 'full_name']
     ordering = ['sample_id']
+    search_fields = ['sample_id', 'full_name']
 
 cgcms_admin_site.register(Sample, SampleAdmin)
 
@@ -48,6 +89,7 @@ cgcms_admin_site.register(Sample, SampleAdmin)
 class CogClassAdmin(admin.ModelAdmin):
     list_display = ['cog_id', 'description']
     ordering = ['cog_id']
+    search_fields = ['cog_id', 'description']
 
 cgcms_admin_site.register(Cog_class, CogClassAdmin)
 
@@ -55,6 +97,7 @@ cgcms_admin_site.register(Cog_class, CogClassAdmin)
 class KeggReactionAdmin(admin.ModelAdmin):
     list_display = ['kegg_id', 'description']
     ordering = ['kegg_id']
+    search_fields = ['kegg_id', 'description']
 
 cgcms_admin_site.register(Kegg_reaction, KeggReactionAdmin)
 
@@ -62,6 +105,7 @@ cgcms_admin_site.register(Kegg_reaction, KeggReactionAdmin)
 class KeggPathwayAdmin(admin.ModelAdmin):
     list_display = ['kegg_id', 'description']
     ordering = ['kegg_id']
+    search_fields = ['kegg_id', 'description']
 
 cgcms_admin_site.register(Kegg_pathway, KeggPathwayAdmin)
 
@@ -69,6 +113,7 @@ cgcms_admin_site.register(Kegg_pathway, KeggPathwayAdmin)
 class KeggOrthologAdmin(admin.ModelAdmin):
     list_display = ['kegg_id', 'description']
     ordering = ['kegg_id']
+    search_fields = ['kegg_id', 'description']
 
 cgcms_admin_site.register(Kegg_ortholog, KeggOrthologAdmin)
 
@@ -76,6 +121,7 @@ cgcms_admin_site.register(Kegg_ortholog, KeggOrthologAdmin)
 class GoTermAdmin(admin.ModelAdmin):
     list_display = ['go_id', 'go_namespace', 'description']
     ordering = ['go_id']
+    search_fields = ['go_id', 'go_namespace', 'description']
 
 cgcms_admin_site.register(Go_term, GoTermAdmin)
 
@@ -83,6 +129,7 @@ cgcms_admin_site.register(Go_term, GoTermAdmin)
 class CazyFamilyAdmin(admin.ModelAdmin):
     list_display = ['cazy_id', 'description']
     ordering = ['cazy_id']
+    search_fields = ['cazy_id', 'description']
 
 cgcms_admin_site.register(Cazy_family, CazyFamilyAdmin)
 
@@ -90,6 +137,7 @@ cgcms_admin_site.register(Cazy_family, CazyFamilyAdmin)
 class EcNumberAdmin(admin.ModelAdmin):
     list_display = ['ec_number', 'description']
     ordering = ['ec_number']
+    search_fields = ['ec_number', 'description']
 
 cgcms_admin_site.register(Ec_number, EcNumberAdmin)
 
@@ -97,6 +145,7 @@ cgcms_admin_site.register(Ec_number, EcNumberAdmin)
 class TcFamilyAdmin(admin.ModelAdmin):
     list_display = ['tc_id', 'description']
     ordering = ['tc_id']
+    search_fields = ['tc_id', 'description']
 
 cgcms_admin_site.register(Tc_family, TcFamilyAdmin)
 
@@ -104,6 +153,7 @@ cgcms_admin_site.register(Tc_family, TcFamilyAdmin)
 class OrthologGroupAdmin(admin.ModelAdmin):
     list_display = ['eggnog_id', 'taxon']
     ordering = ['eggnog_id']
+    search_fields = ['eggnog_id', 'taxon']
 
 cgcms_admin_site.register(Ortholog_group, OrthologGroupAdmin)
 
@@ -111,6 +161,7 @@ cgcms_admin_site.register(Ortholog_group, OrthologGroupAdmin)
 class EggnogDescriptionAdmin(admin.ModelAdmin):
     list_display = ['fingerprint', 'description']
     ordering = ['fingerprint']
+    search_fields = ['fingerprint', 'description']
 
 cgcms_admin_site.register(Eggnog_description, EggnogDescriptionAdmin)
 
@@ -118,6 +169,7 @@ cgcms_admin_site.register(Eggnog_description, EggnogDescriptionAdmin)
 class StrainMetadataAdmin(admin.ModelAdmin):
     list_display = ['strain', 'key', 'source']
     ordering = ['strain', 'key']
+    search_fields = ['strain', 'key', 'source']
 
 cgcms_admin_site.register(Strain_metadata, StrainMetadataAdmin)
 
@@ -125,6 +177,7 @@ cgcms_admin_site.register(Strain_metadata, StrainMetadataAdmin)
 class SampleMetadataAdmin(admin.ModelAdmin):
     list_display = ['sample', 'key', 'source']
     ordering = ['sample', 'key']
+    search_fields = ['sample', 'key', 'source']
 
 cgcms_admin_site.register(Sample_metadata, SampleMetadataAdmin)
 
@@ -132,6 +185,7 @@ cgcms_admin_site.register(Sample_metadata, SampleMetadataAdmin)
 class ProteinAdmin(admin.ModelAdmin):
     list_display = ['name', 'protein_hash', 'length']
     ordering = ['protein_hash']
+    search_fields = ['name', 'protein_hash', 'length']
 
 cgcms_admin_site.register(Protein, ProteinAdmin)
 
@@ -139,6 +193,7 @@ cgcms_admin_site.register(Protein, ProteinAdmin)
 class AnnotationAdmin(admin.ModelAdmin):
     list_display = ['gene_id', 'key', 'value', 'source']
     ordering = ['gene_id', 'key']
+    search_fields = ['gene_id__locus_tag', 'key', 'value', 'source']
 
 cgcms_admin_site.register(Annotation, AnnotationAdmin)
 
@@ -146,6 +201,7 @@ cgcms_admin_site.register(Annotation, AnnotationAdmin)
 class OperonAdmin(admin.ModelAdmin):
     list_display = ['name', 'genome', 'contig', 'start', 'end']
     ordering = ['genome', 'name']
+    search_fields = ['name', 'genome', 'contig']
 
 cgcms_admin_site.register(Operon, OperonAdmin)
 
@@ -153,6 +209,7 @@ cgcms_admin_site.register(Operon, OperonAdmin)
 class SiteAdmin(admin.ModelAdmin):
     list_display = ['name', 'genome', 'contig', 'start', 'end', 'type']
     ordering = ['genome', 'name']
+    search_fields = ['name', 'genome', 'contig', 'type']
 
 cgcms_admin_site.register(Site, SiteAdmin)
 
@@ -160,6 +217,7 @@ cgcms_admin_site.register(Site, SiteAdmin)
 class RegulonAdmin(admin.ModelAdmin):
     list_display = ['name', 'genome']
     ordering = ['genome', 'name']
+    search_fields = ['name', 'genome']
 
 cgcms_admin_site.register(Regulon, RegulonAdmin)
 
@@ -167,6 +225,7 @@ cgcms_admin_site.register(Regulon, RegulonAdmin)
 class ConfigAdmin(admin.ModelAdmin):
     list_display = ['param', 'value']
     ordering = ['param']
+    search_fields = ['param', 'value']
 
 cgcms_admin_site.register(Config, ConfigAdmin)
 
@@ -174,12 +233,15 @@ cgcms_admin_site.register(Config, ConfigAdmin)
 class ContigAdmin(admin.ModelAdmin):
     list_display = ['contig_id', 'name', 'genome', 'size']
     ordering = ['genome', 'contig_id']
+    search_fields = ['contig_id', 'name', 'genome']
 
 cgcms_admin_site.register(Contig, ContigAdmin)
 
 
 class ChangeLogAdmin(admin.ModelAdmin):
     list_display = ['action', 'timestamp']
+    list_filter = ['timestamp']
     ordering = ['timestamp']
 
 cgcms_admin_site.register(ChangeLog, ChangeLogAdmin)
+
