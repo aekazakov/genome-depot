1e591349a5627aef7e2405c6fb9ff47957e9f7a1
Alexey Kazakov <aekazakov@lbl.gov>
2022-04-21 15:44:43 -0700

    Updated conserved regulon page

--- genomebrowser/browser/conserved_regulon.py
+++ genomebrowser/browser/conserved_regulon.py
@@ -1,17 +1,18 @@
 from .models import *
 from collections import defaultdict
 from browser.dataimport.annotator import autovivify
+from django.urls import reverse
 
 
-def get_lowest_level_og(eggnog_groups):
+def get_lowest_level_og(protein):
     result = None
     RANKS = ('species', 'genus', 'family', 'order', 'class', 'phylum', 'superkingdom', 'norank')
     eggnog_ranks = {}
-    for eggnog_group in eggnog_groups.all():
+    for eggnog_group in protein.ortholog_groups.all():
         eggnog_ranks[eggnog_group.taxon.rank] = eggnog_group
     for rank in RANKS:
         if rank in eggnog_ranks:
-            result = eggnog_group
+            result = eggnog_ranks[rank]
             break
     return result
     
@@ -23,9 +24,9 @@ def build_conserved_regulon(og_id):
     except Ortholog_group.DoesNotExist:
         raise Http404('Ortholog group not found: ' + og_id)
     
-    regulators = Gene.objects.filter(protein__ortholog_groups__id=og.id).select_related('protein', 'genome', 'genome__strain__taxon')
-    regulons = Regulon.objects.filter(regulators__id__in=[item.id for item in regulators]).select_related('genome')
-    sites = Site.objects.filter(regulon__id__in=[item.id for item in regulons]).select_related('genome', 'contig', 'regulon').prefetch_related('genes', 'operons')
+    regulators = Gene.objects.filter(protein__ortholog_groups__id=og.id).select_related('protein', 'genome')
+    regulons = Regulon.objects.filter(regulators__id__in=[item.id for item in regulators]).select_related('genome', 'genome__taxon').prefetch_related('regulators', 'regulators__genome')
+    sites = Site.objects.filter(regulon__id__in=[item.id for item in regulons]).select_related('genome', 'contig', 'regulon').prefetch_related('genes', 'genes__genome', 'operons', 'operons__genome', 'operons__contig', 'operons__genes', 'operons__genes__genome', 'operons__genes__protein', 'operons__genes__protein__ortholog_groups', 'operons__genes__protein__ortholog_groups__taxon', 'genes__protein', 'genes__protein__ortholog_groups', 'genes__protein__ortholog_groups__taxon')
     context['regulator_og'] = og
     context['regulators'] = regulators
     context['regulons'] = regulons
@@ -33,51 +34,77 @@ def build_conserved_regulon(og_id):
     
     eggnogs = autovivify(2, list)
     regulon_labels = set()
-    eggnog_labels = defaultdict(dict)
+    eggnog_labels = {}
+    eggnog_functions = defaultdict(dict)
+    genes_added = set()
+    operons_added = set()
+    fake_id = 0
+    
     for site in sites:
         regulon_label = site.regulon.name + '[' + site.genome.name + ']'
         regulon_labels.add(regulon_label)
         for gene in site.genes.all():
-            if not gene.protein:
+            if gene.id in genes_added:
                 continue
-            eggnog_group = get_lowest_level_og(gene.protein.ortholog_groups.select_related('taxon'))
-            if eggnog_group is not None:
-                eggnogs[eggnog_group.eggnog_id + '[' + eggnog_group.taxon.name + ']'][regulon_label].append(gene.locus_tag)
-                eggnog_labels[eggnog_group.eggnog_id + '[' + eggnog_group.taxon.name + ']'][gene.function] = ''
-        for operon in site.operons.all():
-            for gene in operon.genes.all():
-                if not gene.protein:
-                    continue
-                eggnog_group = get_lowest_level_og(gene.protein.ortholog_groups.select_related('taxon'))
-                if eggnog_group is not None:
-                    eggnogs[eggnog_group.eggnog_id + '[' + eggnog_group.taxon.name + ']'][regulon_label].append(gene.locus_tag)
-                    eggnog_labels[eggnog_group.eggnog_id + '[' + eggnog_group.taxon.name + ']'][gene.function] = ''
-    
-    context['gene_table_header'] = sorted(list(regulon_labels))
-    
-    gene_table = []
-    gene_table.append('			<table>')
-    gene_table.append('				<thead>')
-    gene_table.append('					<tr>')
-    gene_table.append('						<th>EggNOG family</th>')
-    for regulon_label in sorted(list(regulon_labels)):
-        gene_table.append('						<th>' + regulon_label + '</th>')
-    gene_table.append('						<th>Function</th>')
-    gene_table.append('					</tr>')
-    gene_table.append('				</thead>')
-    gene_table.append('				<tbody>')
-    for eggnog_label in eggnogs:
-        gene_table.append('					<tr>')
-        gene_table.append('					    <td>' + eggnog_label + '</td>')
-        gene_functions = set()
-        for regulon_label in sorted(list(regulon_labels)):
-            if regulon_label in eggnogs[eggnog_label]:
-                gene_table.append('					    <td>' + ';'.join(eggnogs[eggnog_label][regulon_label]) + '</td>')
+            genes_added.add(gene.id)
+            if gene.protein is None:
+                current_id = str(fake_id)
+                fake_id += 1
+                eggnogs[current_id][regulon_label].append('<a href="' + reverse('genedetails', args=(gene.genome.name, gene.locus_tag)) + '" title="' + gene.locus_tag + '">')
+                eggnog_functions[current_id][gene.function] = ''
+                eggnog_labels[current_id] = 'No EggNOG ID'
             else:
-                gene_table.append('					    <td>&nbsp;</td>')
-        gene_table.append('					    <td>' + ';'.join(sorted(eggnog_labels[eggnog_label].keys())) + '</td>')
-        gene_table.append('					</tr>')
-    gene_table.append('				</tbody>')
-    gene_table.append('			</table>')
-    context['gene_table'] = '\n'.join(gene_table)
+                eggnog_group = get_lowest_level_og(gene.protein)
+                if eggnog_group is not None:
+                    eggnogs[eggnog_group.id][regulon_label].append('<a href="' + reverse('genedetails', args=(gene.genome.name, gene.locus_tag)) + '" title="' + gene.locus_tag + '">')
+                    eggnog_functions[eggnog_group.id][gene.function] = ''
+                    eggnog_labels[eggnog_group.id] = '<a href="' + reverse('searchgene') + "?og=" + str(eggnog_group.id) + '">' + eggnog_group.eggnog_id + '[' + eggnog_group.taxon.name + ']</a>'
+                else:
+                    current_id = str(fake_id)
+                    fake_id += 1
+                    eggnogs[current_id][regulon_label].append('<a href="' + reverse('genedetails', args=(gene.genome.name, gene.locus_tag)) + '" title="' + gene.locus_tag + '">')
+                    eggnog_functions[current_id][gene.function] = ''
+                    eggnog_labels[current_id] = 'No EggNOG ID'
+        for operon in site.operons.all():
+            if operon.id in operons_added:
+                continue
+            operons_added.add(operon.id)
+            for gene in operon.genes.all():
+                if gene.id in genes_added:
+                    continue
+                genes_added.add(gene.id)
+                if not gene.protein:
+                    current_id = str(fake_id)
+                    fake_id += 1
+                    eggnogs[current_id][regulon_label].append('<a href="' + reverse('genedetails', args=(gene.genome.name, gene.locus_tag)) + '" title="' + gene.locus_tag + '">')
+                    eggnog_functions[current_id][gene.function] = ''
+                    eggnog_labels[current_id] = 'No EggNOG ID'
+                else:
+                    eggnog_group = get_lowest_level_og(gene.protein)
+                    if eggnog_group is not None:
+                        eggnogs[eggnog_group.id][regulon_label].append('<a href="' + reverse('genedetails', args=(gene.genome.name, gene.locus_tag)) + '" title="' + gene.locus_tag + '">')
+                        eggnog_functions[eggnog_group.id][gene.function] = ''
+                        eggnog_labels[eggnog_group.id] = '<a href="' + reverse('searchgene') + "?og=" + str(eggnog_group.id) + '">' + eggnog_group.eggnog_id + '[' + eggnog_group.taxon.name + ']</a>'
+                    else:
+                        current_id = str(fake_id)
+                        fake_id += 1
+                        eggnogs[current_id][regulon_label].append('<a href="' + reverse('genedetails', args=(gene.genome.name, gene.locus_tag)) + '" title="' + gene.locus_tag + '">')
+                        eggnog_functions[current_id][gene.function] = ''
+                        eggnog_labels[current_id] = 'No EggNOG ID'
+
+    context['gene_table_header'] = sorted(list(regulon_labels))
+    gene_table_rows = []
+    
+    for eggnog_id in eggnogs:
+        table_row = ['<td>' + eggnog_labels[eggnog_id] + '</td>']
+        for regulon_label in sorted(list(regulon_labels)):
+            if regulon_label in eggnogs[eggnog_id]:
+                table_row.append('<td class="filledcell">' + '<br/>'.join([item + str(index + 1) + '</a>' for index, item in enumerate(eggnogs[eggnog_id][regulon_label])]) + '</td>')
+            else:
+                table_row.append('<td>&nbsp;</td>')
+        table_row.append('<td>' + '; '.join(sorted(eggnog_functions[eggnog_id].keys())) + '</td>')
+        gene_table_rows.append(table_row)
+    if gene_table_rows:
+        context['gene_table_rows'] = gene_table_rows
+
     return context
